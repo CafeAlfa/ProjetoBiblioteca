d7e09f5b861fb6e29fb4e40dec6cca70033449b3
CafeAlfa <carlosfelipe.falcao@gmail.com>
2021-09-07 17:04:15 -0300

    Agora pode-se pesquisar por Usuarios e por Livros na listagem dde emprestimos

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -30,11 +30,54 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
-        {
-            using(BibliotecaContext bc = new BibliotecaContext())
+    public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                using(BibliotecaContext bc = new BibliotecaContext())
+                {
+                    IQueryable<Emprestimo> consulta;
+
+                    if(filtro!=null)
+                    {
+                        switch(filtro.TipoFiltro)
+                        {
+                        case "Usuario":
+                        consulta = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
+                        break;
+
+                        case "Livro":
+                        List<Livro> LivrosFiltrados = bc.Livros.Where(l => l.Titulo.Contains(filtro.Filtro)).ToList();
+                        
+                        List<int>LivrosIds = new List<int>();
+                        
+                        for (int i= 0; i<LivrosFiltrados.Count; i++)
+                        
+                        {LivrosIds.Add(LivrosFiltrados[i].Id);}
+
+                        consulta = bc.Emprestimos.Where(e => LivrosIds.Contains(e.LivroId));
+                        var debug = consulta.ToList();
+                        break;
+
+                        default:
+                        consulta = bc.Emprestimos;
+                        break;
+                    
+                    }
+
+                }
+                 
+                else
+                {
+                    consulta = bc.Emprestimos;
+                }
+                
+            List<Emprestimo>ListaConsulta = consulta.OrderBy(e => e.DataEmprestimo).ToList();
+
+            for (int i = 0; i< ListaConsulta.Count; i++) 
+            {
+                ListaConsulta[i].Livro = bc.Livros.Find(ListaConsulta[i].LivroId);
+            }  
+ 
+                return ListaConsulta;
             }
         }
 
